4904387a6ec3b298bfa2e3fe4fa643c1db8252d3
Rakesh Gupta <rakgupta@adobe.com>
2019-07-22 19:00:18 +0530

    Updates

---
+++ precache-manifest.e7951242487d7bce89cec28ca014da17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d155e81f234131318f290b90d90262",
+    "url": "/sp/index.html"
+  },
+  {
+    "revision": "50da89aabf5e7a77d027",
+    "url": "/sp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "50da89aabf5e7a77d027",
+    "url": "/sp/static/js/main.18ba109d.chunk.js"
+  },
+  {
+    "revision": "1d2dd143fddad4096c19",
+    "url": "/sp/static/js/runtime~main.96ce5f54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sp/static/media/logo.5d5d9eef.svg"
+  }
+]);
